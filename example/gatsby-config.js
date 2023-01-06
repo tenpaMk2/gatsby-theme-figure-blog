1dab4d8d27ffbf44cb543110959b0883f27abde5
tenpaMk2 <wavyroom@gmail.com>
2023-01-06 23:57:04 +0900

    :sparkles: tailwindcss is ready on example-site.

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-theme-figure-blog`, options: {} }],
-}
+  plugins: [
+    "gatsby-plugin-postcss",
+    { resolve: `gatsby-theme-figure-blog`, options: {} },
+  ],
+};
