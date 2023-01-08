fc828dc04ede7ff4c95f395d885da3d3f8b63e2f
tenpaMk2 <wavyroom@gmail.com>
2023-01-08 23:00:25 +0900

    :sparkles: add style for markdown post.

--- gatsby-theme-figure-blog/src/templates/blog-post.js
+++ gatsby-theme-figure-blog/src/templates/blog-post.js
@@ -1,53 +1,24 @@
 import * as React from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 
 const BlogPostTemplate = ({
   data: { previous, next, site, markdownRemark: post },
-  location,
 }) => {
   return (
-    <>
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-        </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-      </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </>
+    <article
+      className="blog-post prose bg-slate-50 dark:prose-invert dark:bg-slate-700"
+      itemScope
+      itemType="http://schema.org/Article"
+    >
+      <header>
+        <h1 itemProp="headline">{post.frontmatter.title}</h1>
+        <p>{post.frontmatter.date}</p>
+      </header>
+      <section
+        dangerouslySetInnerHTML={{ __html: post.html }}
+        itemProp="articleBody"
+      />
+    </article>
   );
 };
 
