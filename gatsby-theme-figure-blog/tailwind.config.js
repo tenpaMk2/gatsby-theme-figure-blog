d8783d5c072ee184a447d230f8948b26e36a3098
tenpaMk2 <wavyroom@gmail.com>
2023-01-08 22:13:16 +0900

    :bug: fix that tailwind does not work properly in `src/templates/*` .

--- gatsby-theme-figure-blog/tailwind.config.js
+++ gatsby-theme-figure-blog/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    `${__dirname}/src/pages/**/*.{js,jsx,ts,tsx}`,
     `${__dirname}/src/components/**/*.{js,jsx,ts,tsx}`,
+    `${__dirname}/src/pages/**/*.{js,jsx,ts,tsx}`,
+    `${__dirname}/src/templates/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
     extend: {},
