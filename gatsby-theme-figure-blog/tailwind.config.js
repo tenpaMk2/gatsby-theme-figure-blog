35c212fba49c5917c1c6678ebe3a403c6c974efb
tenpaMk2 <wavyroom@gmail.com>
2023-01-09 16:33:42 +0900

    :wrench: customize tailwind-typography css.

--- gatsby-theme-figure-blog/tailwind.config.js
+++ gatsby-theme-figure-blog/tailwind.config.js
@@ -6,7 +6,31 @@ module.exports = {
     `${__dirname}/src/templates/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            blockquote: {
+              backgroundColor: `rgba(128, 128, 128, 0.2)`,
+              overflow: `hidden`,
+              quotes: null,
+            },
+            "blockquote p:first-of-type::before": {
+              content: null,
+            },
+            "blockquote p:last-of-type::after": {
+              content: null,
+            },
+            "code::before": {
+              content: null,
+            },
+            "code::after": {
+              content: null,
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require(`@tailwindcss/typography`)],
 };
