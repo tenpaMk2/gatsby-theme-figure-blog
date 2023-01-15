dc77edfabfa32d60a184e3a2db8d367595b26641
tenpaMk2 <wavyroom@gmail.com>
2023-01-15 22:22:32 +0900

    :wrench: rename theme path.

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-postcss",
-    { resolve: `gatsby-theme-figure-blog`, options: {} },
+    { resolve: `@tenpamk2/gatsby-theme-figure-blog`, options: {} },
   ],
 };
