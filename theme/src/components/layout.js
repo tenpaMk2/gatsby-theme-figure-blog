acd677139b6e6e149215a8a68cf592fee0d276e9
tenpaMk2 <wavyroom@gmail.com>
2023-01-20 07:58:59 +0900

    :sparkles: add header image and nav.

--- theme/src/components/layout.js
+++ theme/src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { graphql, Link, useStaticQuery } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Layout = ({ children }) => {
   const {
@@ -20,8 +21,32 @@ const Layout = ({ children }) => {
 
   return (
     <div>
-      <header>
-        <Link to="/">{title}</Link>
+      <header className="relative">
+        {/* See [gatsby-plugin-image doc](https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-plugin-image/#shared-props) */}
+        <StaticImage
+          src="../images/header.jpg"
+          alt="header image"
+          // See [gatsby-plugin-image doc](https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-plugin-image/#layout)
+          layout="fullWidth"
+          className="h-72"
+        />
+        <div className="absolute top-0 left-0 text-white">
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/archives/">Archives</Link>
+            <Link to="/about/">About</Link>
+            <Link to="/apps/">Apps</Link>
+          </nav>
+          <nav>
+            <Link to="/">RSS</Link>
+            <Link to="/">RSSフィギュア</Link>
+            <Link to="/">RSSドール</Link>
+            <Link to="/">検索</Link>
+          </nav>
+        </div>
+        <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-sans text-6xl font-light text-white drop-shadow">
+          <Link to="/">{title}</Link>
+        </h1>
       </header>
       <main>{children}</main>
       <footer>
