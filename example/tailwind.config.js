71dd254f004f56d60e8fa83b36b68e1b580e0ef8
tenpaMk2 <wavyroom@gmail.com>
2023-01-22 22:46:49 +0900

    :wrench: add tailwind `content` at `example` .

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/@tenpamk2/gatsby-theme-figure-blog/components/**/*.{js,jsx,ts,tsx}",
+    "./src/@tenpamk2/gatsby-theme-figure-blog/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/@tenpamk2/gatsby-theme-figure-blog/templates/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
