c8979e502e978b5abc7461180c6b54c568b9cb93
tenpaMk2 <wavyroom@gmail.com>
2023-01-29 21:45:20 +0900

    :wrench: Limits image height and change image quality.

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -17,11 +17,13 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 900,
+              maxWidth: 1024, // sync the value of `max-w-screen-lg` by Tailwind CSS.
               linkImagesToOriginal: true,
               withWebp: true,
               withAvif: true,
-              quality: 80,
+              quality: 90,
+              wrapperStyle: (image) =>
+                `max-width:${(image.aspectRatio * 100).toFixed(2)}vh`, // See [issue: Can't specify max-width](https://github.com/gatsbyjs/gatsby/issues/15578) .
             },
           },
           `gatsby-remark-prismjs`,
