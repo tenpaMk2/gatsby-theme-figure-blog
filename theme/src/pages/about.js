7fcec5e1b9baa2b4bbc720083edb6a641ef015ba
tenpaMk2 <wavyroom@gmail.com>
2023-02-11 19:33:37 +0900

    :sparkles: add a description in the about page.

--- theme/src/pages/about.js
+++ theme/src/pages/about.js
@@ -1,16 +1,38 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
 
-const AboutPage = () => {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold borderline bg-red-500">About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p className="bg-red-500">HOGEEEEEEEEEEEE❗</p>
-      <div className="bg-red-500">fua</div>
-    </main>
-  );
-};
+// TODO: Use markdown post.
+const AboutPage = () => (
+  <Layout>
+    <article
+      className="w-full rounded-xl bg-slate-700 p-8"
+      itemScope
+      itemType="http://schema.org/Article"
+    >
+      <section className="prose prose-invert max-w-none">
+        <header>
+          <h1 itemProp="headline" className="mb-2">
+            This is an About page.
+          </h1>
+          <time dateTime="2023-02-11 19:24">2023-02-11 19:24</time>
+          <hr className="my-2 border border-slate-500" />
+        </header>
+        <section itemProp="articleBody">
+          <p>Shadow `about.js` however you like❗</p>
+          <p>
+            Copy
+            `node_modules/@tenpamk2/gatsby-theme-figure-blog/src/pages/about.js`
+            to `src/@tenpamk2/gatsby-theme-figure-blog/pages/about.js` .
+          </p>
+          <p>Edit `src/@tenpamk2/gatsby-theme-figure-blog/pages/about.js` .</p>
+        </section>
+      </section>
+      <footer>
+        <hr className="my-2 border border-slate-500" />
+      </footer>
+    </article>
+  </Layout>
+);
 
 export const Head = () => <title>About Me</title>;
 
