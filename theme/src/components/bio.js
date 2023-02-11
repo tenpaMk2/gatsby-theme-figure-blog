2546e8d409f6e6c5f163d649cd5a90c96141e27a
tenpaMk2 <wavyroom@gmail.com>
2023-02-11 18:53:38 +0900

    :bug: fix camelCase attributes.

--- theme/src/components/bio.js
+++ theme/src/components/bio.js
@@ -77,28 +77,28 @@ const Bio = () => {
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               stroke="currentColor"
-              fill-opacity="0"
+              fillOpacity="0"
               viewBox="0 0 24 24"
               className="h-6 w-6"
             >
               <title>Instagram</title>
               <path
                 d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>
               <path
                 d="M12 15.5C13.933 15.5 15.5 13.933 15.5 12C15.5 10.067 13.933 8.5 12 8.5C10.067 8.5 8.5 10.067 8.5 12C8.5 13.933 10.067 15.5 12 15.5Z"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>
               <path
                 d="M17.6361 7H17.6477"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>
             </svg>
           </a>
