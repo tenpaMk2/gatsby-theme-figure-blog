24291a320b0a017724aea60f78b10975191d59db
tenpaMk2 <wavyroom@gmail.com>
2023-02-13 15:39:01 +0900

    :bulb: fix typo in comments.

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `tenpaMk2's blog`,
     description: `フィギュア、ドール、プログラミング、バイク、音楽についてしゃべる`,
-    siteUrl: `https://tenpamk2-blog.netlify.app`, // must not end with a `/` . See [Gatsby doc](https://www.gatsbyjs.com/docs/location-data-from-props/#getting-the-absolute-url-of-a-page)
+    siteUrl: `https://tenpamk2-blog.netlify.app`, // Must not end with a `/` . See [Gatsby doc](https://www.gatsbyjs.com/docs/location-data-from-props/#getting-the-absolute-url-of-a-page)
     author: {
       name: `tenpaMk2`,
       summary: `インドアクソオタク。`,
@@ -12,7 +12,7 @@ module.exports = {
       instagram: `tenpamk2_figure`,
       github: `tenpaMk2`,
     },
-    locale: `ja-JP`, // see [facebook localization doc](https://developers.facebook.com/docs/javascript/internationalization#locales).
+    locale: `ja-JP`, // See [Facebook localization doc](https://developers.facebook.com/docs/javascript/internationalization#locales).
   },
   plugins: [
     "gatsby-plugin-postcss",
@@ -20,7 +20,7 @@ module.exports = {
       resolve: `@tenpamk2/gatsby-theme-figure-blog`,
       options: {
         basePath: `b`,
-        locale: `ja-JP`, // See [`Intl.Locale in MDN](https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale).
+        locale: `ja-JP`, // See [`Intl.Locale` in MDN](https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale).
         postPath: `p`,
       },
     },
