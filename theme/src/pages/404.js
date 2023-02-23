9d0ba4871c4a89eabe274babd10f5619d68fb3a3
tenpaMk2 <wavyroom@gmail.com>
2023-02-24 00:30:02 +0900

    :zap: Make the font size smaller at the 404 page.

--- theme/src/pages/404.js
+++ theme/src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="flex h-80 basis-full flex-wrap content-center items-center gap-4 rounded-xl bg-slate-700 p-6 text-center text-6xl font-bold tracking-widest">
+    <div className="flex h-80 basis-full flex-wrap content-center items-center gap-4 rounded-xl bg-slate-700 p-6 text-center text-5xl font-bold tracking-widest">
       <p className="basis-full">⚠404⚠</p>
       <p className="basis-full">⚠Errors⚠</p>
     </div>
