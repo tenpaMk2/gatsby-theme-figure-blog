9a2c55c1cbe70a8830a308c408749f4550b1f114
tenpaMk2 <wavyroom@gmail.com>
2023-02-26 10:50:07 +0900

    :bulb: Add a description about `Border` .

--- theme/src/components/border.js
+++ theme/src/components/border.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 
+/**
+ * This component works as border when used as flex-items and `align-items: stretch` .
+ */
 export const Border = () => (
   <div className="border-r border-b border-slate-500" />
 );
