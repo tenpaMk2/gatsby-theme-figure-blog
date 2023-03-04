8d495f3010bea91afa62459babaa296a3eebca6c
tenpaMk2 <wavyroom@gmail.com>
2023-03-05 00:53:39 +0900

    :recycle: Split SVG code to each component.

--- theme/src/components/bio.js
+++ theme/src/components/bio.js
@@ -2,6 +2,22 @@ import * as React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import { SidebarLayout } from "./sidebar-layout";
+import { Twitter } from "./svgs/twitter";
+import { Instagram } from "./svgs/instagram";
+import { GitHub } from "./svgs/github";
+
+const IconLink = ({ label, href, children }) => (
+  <a
+    aria-label={label}
+    className="block h-12 w-12 rounded-full bg-sky-500 p-2 text-white transition duration-200 hover:bg-sky-400"
+    target="_blank"
+    rel="noopener"
+    href={href}
+    draggable="false"
+  >
+    {children}
+  </a>
+);
 
 export const Bio = () => {
   const {
@@ -45,100 +61,18 @@ export const Bio = () => {
         </div>
         <p className="">{summary}</p>
         <div className="flex flex-wrap justify-center gap-2">
-          <a
-            className="inline-flex items-center rounded-full bg-sky-500 p-3 text-white transition duration-200 hover:bg-sky-400"
-            target="_blank"
-            rel="noopener"
-            href={`https://twitter.com/${twitter}/`}
-            aria-label="Twitter"
-            draggable="false"
-          >
-            {/* [Get from.](https://www.svgrepo.com/svg/501411/twitter-boxed) */}
-            <svg
-              aria-hidden="true"
-              className="h-6 w-6"
-              fill="currentColor"
-              stroke="currentColor"
-              viewBox="0 0 1920 1920"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1920 311.856c-70.701 33.769-146.598 56.47-226.221 66.86 81.317-52.517 143.774-135.529 173.252-234.691-76.236 48.678-160.716 84.028-250.391 103.002-71.718-82.56-174.268-134.06-287.435-134.06-217.75 0-394.165 189.966-394.165 424.206 0 33.318 3.614 65.619 10.165 96.678C617.9 616.119 327.304 447.385 133.045 190.67c-33.77 62.57-53.309 135.53-53.309 213.233 0 147.162 91.031 276.818 196.744 353.054-64.602-2.26-157.101-21.46-157.101-53.309v5.648c0 205.327 114.41 376.658 294.55 415.849-32.978 9.487-78.38 14.795-114.409 14.795-25.412 0-55.454-2.71-79.624-7.793 50.26 168.509 193.13 291.163 365.478 294.777-134.852 113.506-306.07 181.383-490.616 181.383-31.85 0-64.038-2.033-94.758-5.873 174.494 120.17 381.176 190.532 603.67 190.532 724.97 0 1121.055-646.136 1121.055-1206.55 0-18.41-.452-36.932-1.356-55.116 77.026-59.746 143.887-134.4 196.631-219.444"
-                fill-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <a
-            className="inline-flex items-center rounded-full bg-sky-500 p-3 text-white transition duration-200 hover:bg-sky-400"
-            target="_blank"
-            rel="noopener"
+          <IconLink label="Twitter" href={`https://twitter.com/${twitter}/`}>
+            <Twitter />
+          </IconLink>
+          <IconLink
+            label="Instagram"
             href={`https://www.instagram.com/${instagram}/`}
-            aria-label="Instagram"
-            draggable="false"
           >
-            {/* [Get from.](https://www.svgrepo.com/svg/497210/instagram) */}
-            <svg
-              aria-hidden="true"
-              className="h-6 w-6"
-              fill="currentColor"
-              fillOpacity="0"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Instagram</title>
-              <path
-                d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-              <path
-                d="M12 15.5C13.933 15.5 15.5 13.933 15.5 12C15.5 10.067 13.933 8.5 12 8.5C10.067 8.5 8.5 10.067 8.5 12C8.5 13.933 10.067 15.5 12 15.5Z"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-              <path
-                d="M17.6361 7H17.6477"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </svg>
-          </a>
-          <a
-            className="inline-flex items-center rounded-full bg-sky-500 p-3 text-white transition duration-200 hover:bg-sky-400"
-            target="_blank"
-            rel="noopener"
-            href={`https://github.com/${github}/`}
-            aria-label="GitHub"
-            draggable="false"
-          >
-            {/* [Get from.](https://www.svgrepo.com/svg/508076/github) */}
-            <svg
-              aria-hidden="true"
-              className="h-6 w-6"
-              fill="currentColor"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>GitHub</title>
-              <path
-                d="M9.29183 21V18.4407L9.3255 16.6219C9.36595 16.0561 9.58639 15.5228 9.94907 15.11C9.95438 15.1039 9.95972 15.0979 9.9651 15.0919C9.9791 15.0763 9.96988 15.0511 9.94907 15.0485V15.0485C7.52554 14.746 5.0005 13.7227 5.0005 9.26749C4.9847 8.17021 5.3427 7.10648 6.00437 6.27215C6.02752 6.24297 6.05103 6.21406 6.07492 6.18545V6.18545C6.10601 6.1482 6.11618 6.09772 6.10194 6.05134C6.10107 6.04853 6.10021 6.04571 6.09935 6.04289C6.0832 5.9899 6.06804 5.93666 6.05388 5.88321C5.81065 4.96474 5.86295 3.98363 6.20527 3.09818C6.20779 3.09164 6.21034 3.08511 6.2129 3.07858C6.22568 3.04599 6.25251 3.02108 6.28698 3.01493V3.01493C6.50189 2.97661 7.37036 2.92534 9.03298 4.07346C9.08473 4.10919 9.13724 4.14609 9.19053 4.18418V4.18418C9.22901 4.21168 9.27794 4.22011 9.32344 4.20716C9.32487 4.20675 9.32631 4.20634 9.32774 4.20593C9.41699 4.18056 9.50648 4.15649 9.59617 4.1337C11.1766 3.73226 12.8234 3.73226 14.4038 4.1337C14.4889 4.1553 14.5737 4.17807 14.6584 4.20199C14.6602 4.20252 14.6621 4.20304 14.6639 4.20356C14.7174 4.21872 14.7749 4.20882 14.8202 4.17653V4.17653C14.8698 4.14114 14.9187 4.10679 14.967 4.07346C16.6257 2.92776 17.4894 2.9764 17.7053 3.01469V3.01469C17.7404 3.02092 17.7678 3.04628 17.781 3.07946C17.7827 3.08373 17.7843 3.08799 17.786 3.09226C18.1341 3.97811 18.1894 4.96214 17.946 5.88321C17.9315 5.93811 17.9159 5.9928 17.8993 6.04723V6.04723C17.8843 6.09618 17.8951 6.14942 17.9278 6.18875C17.9289 6.18998 17.9299 6.19121 17.9309 6.19245C17.9528 6.21877 17.9744 6.24534 17.9956 6.27215C18.6573 7.10648 19.0153 8.17021 18.9995 9.26749C18.9995 13.747 16.4565 14.7435 14.0214 15.015V15.015C14.0073 15.0165 14.001 15.0334 14.0105 15.0439C14.0141 15.0479 14.0178 15.0519 14.0214 15.0559C14.2671 15.3296 14.4577 15.6544 14.5811 16.0103C14.7101 16.3824 14.7626 16.7797 14.7351 17.1754V21"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-              <path
-                d="M4 17C4.36915 17.0523 4.72159 17.1883 5.03065 17.3975C5.3397 17.6068 5.59726 17.8838 5.7838 18.2078C5.94231 18.4962 6.15601 18.7504 6.41264 18.9557C6.66927 19.161 6.96379 19.3135 7.27929 19.4043C7.59478 19.4952 7.92504 19.5226 8.25112 19.485C8.5772 19.4475 8.89268 19.3457 9.17946 19.1855"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-            </svg>
-          </a>
+            <Instagram />
+          </IconLink>
+          <IconLink label="GitHub" href={`https://github.com/${github}/`}>
+            <GitHub />
+          </IconLink>
         </div>
       </div>
     </SidebarLayout>
---
+++ theme/src/components/svgs/instagram.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+/**
+ * The Instagram SVG image taken from [SVG repo](https://www.svgrepo.com/svg/497210/instagram) .
+ */
+export const Instagram = () => (
+  <svg
+    aria-hidden="true"
+    className="h-full w-full"
+    fill="currentColor"
+    fillOpacity="0"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <title>Instagram</title>
+    <path
+      d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M12 15.5C13.933 15.5 15.5 13.933 15.5 12C15.5 10.067 13.933 8.5 12 8.5C10.067 8.5 8.5 10.067 8.5 12C8.5 13.933 10.067 15.5 12 15.5Z"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M17.6361 7H17.6477"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
