bd5b6f3dda6b4fd92fe49e232ea172e35146839a
tenpaMk2 <wavyroom@gmail.com>
2023-03-13 11:19:24 +0900

    :wrench: Allow plugin options to be changed.

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,44 +1,73 @@
-module.exports = {
+module.exports = (themeOptions) => ({
   plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-image`,
+      options: { ...{}, ...themeOptions.optionsGatsbyPluginImage },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: { ...{}, ...themeOptions.optionsGatsbyPluginPostCss },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: { ...{}, ...themeOptions.optionsGatsbyPluginSharp },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `blog`,
-        path: `content/posts`,
+        ...{
+          name: `blog`,
+          path: `content/posts`,
+        },
+        ...themeOptions.optionsGatsbySourceFilesystemForPosts,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `pages`,
-        path: `content/pages`,
+        ...{
+          name: `pages`,
+          path: `content/pages`,
+        },
+        ...themeOptions.optionsGatsbySourceFilesystemForPages,
       },
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        excerpt_separator: `<!-- more -->`,
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1024, // sync the value of `max-w-screen-lg` by Tailwind CSS.
-              linkImagesToOriginal: true,
-              withWebp: true,
-              withAvif: true,
-              quality: 90,
-              wrapperStyle: (image) =>
-                `max-width:${(image.aspectRatio * 100).toFixed(2)}vh`, // See [issue: Can't specify max-width](https://github.com/gatsbyjs/gatsby/issues/15578) .
-              backgroundColor: "transparent",
+        ...{
+          plugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                ...{
+                  maxWidth: 1024, // sync the value of `max-w-screen-lg` by Tailwind CSS.
+                  linkImagesToOriginal: true,
+                  withWebp: true,
+                  withAvif: true,
+                  quality: 90,
+                  wrapperStyle: (image) =>
+                    `max-width:${(image.aspectRatio * 100).toFixed(2)}vh`, // See [issue: Can't specify max-width](https://github.com/gatsbyjs/gatsby/issues/15578) .
+                  backgroundColor: "transparent",
+                },
+                ...themeOptions.gatsbyRemarkImages,
+              },
             },
-          },
-          `gatsby-remark-prismjs`,
-        ],
+            {
+              resolve: `gatsby-remark-prismjs`,
+              options: {
+                ...{},
+                ...themeOptions.optionsGatsbyRemarkPrismjs,
+              },
+            },
+          ],
+        },
+        ...themeOptions.optionsGatsbyTransformerRemark,
       },
     },
-    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-sharp`,
+      options: { ...{}, ...themeOptions.optionsGatsbyTransformerSharp },
+    },
   ],
-};
+});
