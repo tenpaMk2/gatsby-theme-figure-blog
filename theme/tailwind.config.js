f321442dc932ec5f26f7be545c1cad087d42cb04
tenpaMk2 <wavyroom@gmail.com>
2023-03-21 15:08:50 +0900

    :zap: Support `.mjs` .

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    `${__dirname}/src/components/**/*.{js,jsx,ts,tsx}`,
-    `${__dirname}/src/pages/**/*.{js,jsx,ts,tsx}`,
-    `${__dirname}/src/templates/**/*.{js,jsx,ts,tsx}`,
+    `${__dirname}/src/components/**/*.{js,jsx,mjs,ts,tsx}`,
+    `${__dirname}/src/pages/**/*.{js,jsx,mjs,ts,tsx}`,
+    `${__dirname}/src/templates/**/*.{js,jsx,mjs,ts,tsx}`,
   ],
   theme: {
     extend: {
